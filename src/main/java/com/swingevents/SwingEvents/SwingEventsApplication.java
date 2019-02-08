fbb4d41a5b178ef669df3c63f4b0c7af7713264c
Bartek <mequrel@gmail.com>
2019-02-08 20:27:38 +0100

    add filles generate by Spring Boot initializer

---
+++ src/main/java/com/swingevents/SwingEvents/SwingEventsApplication.java
@@ -0,0 +1,14 @@
+package com.swingevents.SwingEvents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwingEventsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwingEventsApplication.class, args);
+	}
+
+}
+
