fa7fefe69a112972992aa33bb17f9bd26dbceb05
bartlomiejs <bartlomiejs@qualtrics.com>
2019-02-11 22:32:12 +0100

    React webapp - first attempt

---
+++ webapp/src/components/DateRange.js
@@ -0,0 +1,14 @@
+import React from "react";
+import moment from "moment";
+
+const DateRange = props => {
+    const start = moment(new Date(props.start));
+    const end = moment(new Date(props.end));
+    return (
+        <p>{start.format('DD')} - {end.format('DD')} {end.format('MMM YYYY')}
+           &nbsp; ({start.format('ddd')} - {end.format('ddd')})
+        </p>
+    )
+};
+
+export default DateRange;
