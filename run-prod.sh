74c9ec350e38ee389b20b745e6686b9471479573
Bartek <mequrel@gmail.com>
2019-02-12 19:05:53 +0100

    prod run script

--- run-prod.sh
+++ run-prod.sh
@@ -7,5 +7,5 @@
 
 docker load -i artifacts/webapp.tar
 docker load -i artifacts/backend.tar
-docker-compose -f docker-compose-prod.yml up -d 
+docker-compose -f docker-compose-prod.yml up -d --force-recreate
 docker-compose -f docker-compose-prod.yml ps 
