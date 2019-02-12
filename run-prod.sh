e985dd3743cf8907aa4bb2f52c7f0f23f6c11990
Bartek <mequrel@gmail.com>
2019-02-12 19:04:08 +0100

    prod run script

--- run-prod.sh
+++ run-prod.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-(cd backend && ./mvnw package)
-(cd webapp && npm run build)
-docker-compose build
-docker-compose up -d 
-docker-compose ps 
+
+# (cd backend && ./mvnw package)
+# (cd webapp && npm run build)
+# docker-compose build
+
+docker load -i artifacts/webapp.tar
+docker load -i artifacts/backend.tar
+docker-compose -f docker-compose-prod.yml up -d 
+docker-compose -f docker-compose-prod.yml ps 
