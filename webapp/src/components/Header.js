4d4dd540c0ea5fa16686391a57fe124ad491cf15
Bartek <mequrel@gmail.com>
2019-02-12 18:32:33 +0100

    everything in polish on the main page

--- webapp/src/components/Header.js
+++ webapp/src/components/Header.js
@@ -14,7 +14,7 @@ const LoggedOutView = props => {
 
         <li className="nav-item">
           <Link to="/login" className="nav-link">
-            Sign in
+            Zaloguj
           </Link>
         </li>
       </ul>
