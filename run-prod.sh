ee95da5614d49042e8accd3eabfacc3e0970c7d1
Bartek <mequrel@gmail.com>
2019-02-23 22:37:22 +0000

    final https working

--- run-prod.sh
+++ run-prod.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-docker load -i artifacts/webapp.tar
+rm -rf prod-nginx/app/
+tar -xvf artifacts/webapp.tar -C prod-nginx
+mv prod-nginx/webapp/build prod-nginx/app
 docker load -i artifacts/backend.tar
+docker-compose -f docker-compose-prod.yml build
 docker-compose -f docker-compose-prod.yml up -d
 docker-compose -f docker-compose-prod.yml ps 
