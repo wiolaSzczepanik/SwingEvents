557d8f963e47e11af6be65d75f599f2d2bb00c01
bartlomiejs <bartlomiejs@qualtrics.com>
2019-02-23 23:16:06 +0100

    deploying webapp without docker

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,6 @@
 (cd backend && ./mvnw package)
 (cd webapp && npm run build)
 docker-compose build
-docker save -o webapp.tar swingevents_web
+tar -cvf webapp.tar ./webapp/build/
 docker save -o backend.tar swingevents_backend
 scp *.tar root@68.183.216.32:/root/SwingEvents/artifacts/
