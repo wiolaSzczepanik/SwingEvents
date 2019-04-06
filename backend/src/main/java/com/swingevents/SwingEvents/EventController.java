3bdd095a2d7cba04ba8646fd8b028e6e406b2aba
Wiola <wiolaszcz@gmail.com>
2019-04-06 20:14:05 +0200

    change version of log4j, add annotation SLF4j and used it to print logs

--- backend/src/main/java/com/swingevents/SwingEvents/EventController.java
+++ backend/src/main/java/com/swingevents/SwingEvents/EventController.java
@@ -1,5 +1,6 @@
 package com.swingevents.SwingEvents;
 
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.io.IOUtils;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
@@ -17,12 +18,13 @@ import java.util.Set;
 
 
 @RestController
+@Slf4j
 public class EventController {
 
     @RequestMapping("/events")
     public List<Event> seeAllEvents(@QueryParam("tag") String tag) throws Exception {
 
-        SwingEventsApplication.LOGGER.info("[SPRING]--SEE ALL EVENTS");
+        log.info("[SPRING]--SEE ALL EVENTS");
 
         List<Event> allEvents = readJSON();
 
