3e411128897fb0168cbc4cde35e5771a231233fe
Wiola <wiolaszcz@gmail.com>
2019-08-28 22:53:30 +0200

    add condition to validate Potancówka event

--- backend/src/main/java/com/swingevents/SwingEvents/admin/AdminController.java
+++ backend/src/main/java/com/swingevents/SwingEvents/admin/AdminController.java
@@ -1,15 +1,13 @@
 package com.swingevents.SwingEvents.admin;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.swingevents.SwingEvents.JsonEvent;
+import com.swingevents.SwingEvents.db.DbEvent;
+import com.swingevents.SwingEvents.db.EventsRepository;
 import com.swingevents.SwingEvents.images.ImageFetchingService;
 import com.swingevents.SwingEvents.upcoming.EventStatus;
 import com.swingevents.SwingEvents.upcoming.UpcomingEvent;
 import lombok.extern.slf4j.Slf4j;
-
-import com.swingevents.SwingEvents.JsonEvent;
-import com.swingevents.SwingEvents.db.DbEvent;
-import com.swingevents.SwingEvents.db.EventsRepository;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -36,12 +34,16 @@ public class AdminController {
     @RequestMapping("/events")
     public ResponseEntity<JsonEvent> addEvent(@RequestBody JsonEvent event) throws Exception {
         log.info(event.toString());
-        if(event.getFacts().get("type").equals(EventType.Potańcówka.toString())){
-            if(event.getFacts().containsKey("venue") && event.getFacts().containsKey("price")) {
+        if (event.getFacts().get("type").equals(EventType.Potańcówka.toString())) {
+            if (event.getFacts().containsKey("venue") &&
+                    event.getFacts().containsKey("price") &&
+                    event.getFacts().containsKey("bands") &&
+                    event.getFacts().containsKey("time") &&
+                    event.getFacts().containsKey("style"))
+            {
                 DbEvent addedEvent = getDbEvent(event);
                 return ResponseEntity.status(HttpStatus.OK).body(addedEvent.toJsonEvent());
-            }
-            else {
+            } else {
                 return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
             }
         }
@@ -56,14 +58,14 @@ public class AdminController {
         return addedEvent;
     }
 
-    @RequestMapping(value = "/events/{id}",method = RequestMethod.GET)
-    public UpcomingEvent getSingleEvent(@PathVariable("id") Long id){
+    @RequestMapping(value = "/events/{id}", method = RequestMethod.GET)
+    public UpcomingEvent getSingleEvent(@PathVariable("id") Long id) {
         DbEvent dbEvent = eventsRepository.findById(id).orElseThrow(() -> new RuntimeException("Not found"));
         return UpcomingEvent.fromDbEvent(dbEvent, mapper);
     }
 
-    @RequestMapping(value = "/events/{id}",method = RequestMethod.PUT)
-    public void editAdminEvent(@PathVariable("id") Long id, @RequestBody UpcomingEvent event){
+    @RequestMapping(value = "/events/{id}", method = RequestMethod.PUT)
+    public void editAdminEvent(@PathVariable("id") Long id, @RequestBody UpcomingEvent event) {
         log.info("Edit event: id: {}, title: {}", id, event.getTitle());
 
         DbEvent dbEvent = DbEvent.fromUpcomingEvent(event, mapper);
@@ -73,8 +75,8 @@ public class AdminController {
     }
 
     //not delete event from db - only change status event to 'deleted'
-    @RequestMapping(value="events/{id}", method = RequestMethod.DELETE)
-    public void changeEventStatusToDelete(@PathVariable("id") Long id){
+    @RequestMapping(value = "events/{id}", method = RequestMethod.DELETE)
+    public void changeEventStatusToDelete(@PathVariable("id") Long id) {
         log.info("Event status with id: {} was change to DELETED", id);
 
         Optional<DbEvent> maybeEvent = eventsRepository.findById(id);
